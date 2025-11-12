96bbc2e98fc7ee6d3ea6c18055bd5a44e8ff3f6e
rahmat <rahmathegar52@gmail.com>
2025-11-13 02:58:21 +0700

    feat: Getter&Setter

--- src/main/java/org/example/Kendaraan.java
+++ src/main/java/org/example/Kendaraan.java
@@ -3,15 +3,40 @@ package org.example;
 public class Kendaraan {
 
 //    ATRIBUTE
-    String jenis;
-    String warna;
-    int kecepatan;
+    private String jenis;
+    private String warna;
+    private int kecepatan;
 
 //    KONSTRUKTOR
     public Kendaraan(String jenis, String warna, int kecepatan){
         this.jenis = jenis;
         this.warna = warna;
         this.kecepatan = kecepatan;
+    }
+
+//    SETTER
+    public void setJenis(String jenis){
+        this.jenis = jenis;
+    }
+
+    public void setWarna(String warna){
+        this.warna = warna;
+    }
+
+    public void setKecepatan(int kecepatan){
+        this.kecepatan = kecepatan;
+    }
+
+//    GETTER
+    public String getJenis(){
+        return jenis;
+    }
+
+    public String getWarna(){
+        return warna;
+    }
+    public int getKecepatan(){
+        return kecepatan;
     }
 
 //    METHOD
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,26 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+            public static void main(String[] args) {
 
 //        BUAT OBJECT
         Kendaraan mobil = new Kendaraan("Mobil", "Hitam", 80);
 
 
-        System.out.println("Jenis mobil\t\t:" + mobil.jenis);
-        System.out.println("Warna mobil\t\t:" + mobil.warna);
-        System.out.println("Kecepatan mobil\t:" + mobil.kecepatan);
+//        System.out.println("Jenis mobil\t\t:" + mobil.jenis);
+//        System.out.println("Warna mobil\t\t:" + mobil.warna);
+//        System.out.println("Kecepatan mobil\t:" + mobil.kecepatan);
+//        mobil.kecepatan = 100;
+
+//        PAKAI GETTER & SETTER
+        System.out.println("Jenis mobil\t\t:" + mobil.getJenis());
+System.out.println("Warna mobil\t\t:" + mobil.getWarna());
+        System.out.println("Kecepatan mobil\t:" + mobil.getKecepatan());
+
+        mobil.setKecepatan(100);
+
+        System.out.println("\nPerubahan");
+        System.out.println("Kecepatan baru \t: " + mobil.getKecepatan());
 
         mobil.nyalakanMesin();
         mobil.majuKedepan();
